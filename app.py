880d7bb598de693219feb046b0f2bbea133e7c71
Hemang Kumar <hemangsk@gmail.com>
2016-12-16 02:59:47 +0530

    index.html: Modularity with AngularJS
    Creates a tab interface using controller.
    Makes sure the index.html page has
    minimal markup and also makes it
    easy for further tabs to get
    incorporated in future
    
    Closes https://github.com/coala/landing/issues/19

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return app.send_static_file('index.html')
 
-# start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
--- static/resources/js/app.js
+++ static/resources/js/app.js
@@ -1,14 +1,50 @@
-var app = angular.module('coala', []);
+(function(){
+	var app = angular.module('coala', []);
 
-app.controller('SnippetController', function(){
-	
-	self = this
-	self.languages = Object.keys(snippets)
-	self.current_snippet = ""
-	self.setLang = function (language) {
-		self.current_snippet = snippets[language]
-	}
-	$(document).ready(function(){
+
+	app.controller('SnippetController', function(){
+
+		self = this
+		self.languages = Object.keys(snippets)
+		self.current_snippet = ""
+		self.setLang = function (language) {
+			self.current_snippet = snippets[language]
+		}
+		$(document).ready(function(){
 			$('select').material_select();
+		})
 	})
-})
+
+	app.controller('TabController', function () {
+		this.tab = 1
+		this.setTab = function (stab) {
+			this.tab = stab
+		}
+		this.isSet = function (stab) {
+			return this.tab == stab
+		}
+	})
+
+	app.directive('home', function () {
+		return {
+			restrict: 'E',
+			templateUrl: '/partials/tabs/home.html'
+		}
+	})
+
+	app.directive('getinvolved', function () {
+		return {
+			restrict: 'E',
+			templateUrl: '/partials/tabs/getinvolved.html'
+		}
+	})
+
+	app.directive('languages', function () {
+		return {
+			restrict: 'E',
+			templateUrl: '/partials/tabs/languages.html'
+		}
+	})
+
+
+})();
